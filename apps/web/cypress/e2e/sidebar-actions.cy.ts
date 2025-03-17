aa6cd520a5bbf8248ea63231f22a372c7d11b810
kayYZ1 <overek7133@gmail.com>
2025-03-17 11:54:25 +0100

    fix(test): kill preview process after test end

--- apps/web/cypress/e2e/sidebar-actions.cy.ts
+++ apps/web/cypress/e2e/sidebar-actions.cy.ts
@@ -1,56 +1,58 @@
-describe("Test sidebar actions", () => {
-  beforeEach(() => {
-    cy.clearAllCookies();
-    cy.clearLocalStorage();
-    cy.clearAllSessionStorage();
-    cy.visit("/");
-  });
+/// <reference types="cypress" />
 
-  it("should open the sidebar and create new folder", () => {
-    cy.get('[data-slot="sheet-trigger"]').should("be.visible").click();
+describe('Test sidebar actions', () => {
+	beforeEach(() => {
+		cy.clearAllCookies();
+		cy.clearLocalStorage();
+		cy.clearAllSessionStorage();
+		cy.visit('/');
+	});
 
-    cy.get('[id="sidebar-action"]', { timeout: 10000 })
-      .should("be.visible")
-      .click();
+	it('should open the sidebar and create new folder', () => {
+		cy.get('[data-slot="sheet-trigger"]').should('be.visible').click();
 
-    cy.get('[id="create-folder"]', { timeout: 10000 })
-      .should("be.visible")
-      .should("contain.text", "Folder")
-      .click();
+		cy.get('[id="sidebar-action"]', { timeout: 10000 })
+			.should('be.visible')
+			.click();
 
-    cy.get('[data-slot="input"]', { timeout: 10000 })
-      .should("be.visible")
-      .type("New Folder Test");
+		cy.get('[id="create-folder"]', { timeout: 10000 })
+			.should('be.visible')
+			.should('contain.text', 'Folder')
+			.click();
 
-    cy.get('[type="submit"]')
-      .should("be.visible")
-      .should("contain.text", "Create")
-      .click();
-  });
+		cy.get('[data-slot="input"]', { timeout: 10000 })
+			.should('be.visible')
+			.type('New Folder Test');
 
-  it("should open the sidebar and create a new note", () => {
-    cy.get('[data-slot="sheet-trigger"]').should("be.visible").click();
+		cy.get('[type="submit"]')
+			.should('be.visible')
+			.should('contain.text', 'Create')
+			.click();
+	});
 
-    cy.get('[id="sidebar-action"]', { timeout: 10000 })
-      .should("be.visible")
-      .click();
+	it('should open the sidebar and create a new note', () => {
+		cy.get('[data-slot="sheet-trigger"]').should('be.visible').click();
 
-    cy.get('[id="create-note"]', { timeout: 10000 })
-      .should("be.visible")
-      .should("contain.text", "Note")
-      .click();
+		cy.get('[id="sidebar-action"]', { timeout: 10000 })
+			.should('be.visible')
+			.click();
 
-    cy.get('[id="title"]', { timeout: 10000 })
-      .should("be.visible")
-      .type("New Note Test");
+		cy.get('[id="create-note"]', { timeout: 10000 })
+			.should('be.visible')
+			.should('contain.text', 'Note')
+			.click();
 
-    cy.get('[id="description"]', { timeout: 10000 })
-      .should("be.visible")
-      .type("Description for New Note Test");
+		cy.get('[id="title"]', { timeout: 10000 })
+			.should('be.visible')
+			.type('New Note Test');
 
-    cy.get('[type="submit"]')
-      .should("be.visible")
-      .should("contain.text", "Create")
-      .click();
-  });
+		cy.get('[id="description"]', { timeout: 10000 })
+			.should('be.visible')
+			.type('Description for New Note Test');
+
+		cy.get('[type="submit"]')
+			.should('be.visible')
+			.should('contain.text', 'Create')
+			.click();
+	});
 });
