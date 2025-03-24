e3394708e063d2afb6778510caa90df35ab2e49f
kayYZ1 <overek7133@gmail.com>
2025-03-24 22:07:45 +0100

    fix: build error with use-ai hook

--- apps/cypress/e2e/model-selection.cy.ts
+++ apps/cypress/e2e/model-selection.cy.ts
@@ -1,68 +1,68 @@
-describe('Test llm model selection', () => {
-	beforeEach(() => {
-		cy.clearAllCookies();
-		cy.clearLocalStorage();
-		cy.clearAllSessionStorage();
-		cy.visit('/settings');
-	});
+describe("Test llm model selection", () => {
+  beforeEach(() => {
+    cy.clearAllCookies();
+    cy.clearLocalStorage();
+    cy.clearAllSessionStorage();
+    cy.visit("/settings");
+  });
 
-	it('should select the DeepSeekR1 model', () => {
-		cy.get('#model-select').click();
-		cy.contains('[data-slot="select-item"]', 'DeepSeek R1')
-			.should('be.visible')
-			.click();
-		cy.window().then((win) => {
-			expect(win.localStorage.getItem('model')).to.equal(
-				'deepseek/deepseek-r1:free'
-			);
-		});
-	});
+  it("should select the DeepSeekR1 model", () => {
+    cy.get("#model-select").click();
+    cy.contains('[data-slot="select-item"]', "DeepSeek R1")
+      .should("be.visible")
+      .click();
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem("model")).to.equal(
+        "deepseek/deepseek-r1:free",
+      );
+    });
+  });
 
-	it('should select the DeepSeekChat model', () => {
-		cy.get('#model-select').click();
-		cy.contains('[data-slot="select-item"]', 'DeepSeek V3')
-			.should('be.visible')
-			.click();
-		cy.window().then((win) => {
-			expect(win.localStorage.getItem('model')).to.equal(
-				'deepseek/deepseek-chat:free'
-			);
-		});
-	});
+  it("should select the DeepSeekChat model", () => {
+    cy.get("#model-select").click();
+    cy.contains('[data-slot="select-item"]', "DeepSeek V3")
+      .should("be.visible")
+      .click();
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem("model")).to.equal(
+        "deepseek/deepseek-chat:free",
+      );
+    });
+  });
 
-	it('should select the Mistral model', () => {
-		cy.get('#model-select').click();
-		cy.contains('[data-slot="select-item"]', 'Mistral')
-			.should('be.visible')
-			.click();
-		cy.window().then((win) => {
-			expect(win.localStorage.getItem('model')).to.equal(
-				'mistralai/mistral-7b-instruct:free'
-			);
-		});
-	});
+  it("should select the Mistral model", () => {
+    cy.get("#model-select").click();
+    cy.contains('[data-slot="select-item"]', "Mistral")
+      .should("be.visible")
+      .click();
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem("model")).to.equal(
+        "mistralai/mistral-7b-instruct:free",
+      );
+    });
+  });
 
-	it('should select the LLAMA model', () => {
-		cy.get('#model-select').click();
-		cy.contains('[data-slot="select-item"]', 'LLAMA 3.3')
-			.should('be.visible')
-			.click();
-		cy.window().then((win) => {
-			expect(win.localStorage.getItem('model')).to.equal(
-				'meta-llama/llama-3.3-70b-instruct:free'
-			);
-		});
-	});
+  it("should select the LLAMA model", () => {
+    cy.get("#model-select").click();
+    cy.contains('[data-slot="select-item"]', "LLAMA 3.3")
+      .should("be.visible")
+      .click();
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem("model")).to.equal(
+        "meta-llama/llama-3.3-70b-instruct:free",
+      );
+    });
+  });
 
-	it('should select the Gemini model', () => {
-		cy.get('#model-select').click();
-		cy.contains('[data-slot="select-item"]', 'Gemini 2.0')
-			.should('be.visible')
-			.click();
-		cy.window().then((win) => {
-			expect(win.localStorage.getItem('model')).to.equal(
-				'google/gemini-2.0-pro-exp-02-05:free'
-			);
-		});
-	});
+  it("should select the Gemini model", () => {
+    cy.get("#model-select").click();
+    cy.contains('[data-slot="select-item"]', "Gemini 2.0")
+      .should("be.visible")
+      .click();
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem("model")).to.equal(
+        "google/gemini-2.0-pro-exp-02-05:free",
+      );
+    });
+  });
 });
